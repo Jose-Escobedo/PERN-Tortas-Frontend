c6b6cc893b964bc86f8a2b02f438a919c71d056f
+ <escobedo.jose48@outlook.com>
2022-07-10 01:29:24 -0700

    slider component

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import AsadaFries from "./images/asada-fries.jpeg";
+export const SliderItems = [
+  {
+    id: 1,
+    img: `${AsadaFries}`,
+    title: "Mexican Food",
+    desc: "Check out on our new Catering feature!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: `${AsadaFries}`,
+    title: "Mexican Food",
+    desc: "Check out on our new Catering feature!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: `${AsadaFries}`,
+    title: "Mexican Food",
+    desc: "Check out on our new Catering feature!",
+    bg: "fbf0f4",
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <>
       <Announcement />
       <Navbar />
+      <Slider />
     </>
   );
 };

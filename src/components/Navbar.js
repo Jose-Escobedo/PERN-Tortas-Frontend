eafb6b9dd17bd273ebebae4408c9a1c42c790038
+ <escobedo.jose48@outlook.com>
2022-07-09 23:52:31 -0700

    Navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartOutlined from "@material-ui/icons/ShoppingCartOutlined";
+// import LogoImg from "../images/tortaslogo.svg";
 const Navbar = () => {
   return (
     <Container>
@@ -9,11 +12,21 @@ const Navbar = () => {
           <Language>EN</Language>
           <SearchContainer>
             <Input />
-            <FaSearch />
+            <FaSearch style={{ color: "gray", fontSize: "1rem" }} />
           </SearchContainer>
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+          <Logo>Tortas Mexico Studio City</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
@@ -47,9 +60,22 @@ const Input = styled.input`
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  width: 1005;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 1rem;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Wrapper = styled.div`

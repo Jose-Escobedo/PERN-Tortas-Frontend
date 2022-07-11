487579611d7ff2ab35558e8859b30b5d952bc64a
+ <escobedo.jose48@outlook.com>
2022-07-11 01:55:11 -0700

    Product List component

---
+++ src/pages/ProductList.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Title>Tortas</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Items:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Type
+            </Option>
+            <Option>Tortas</Option>
+            <Option>Tacos</Option>
+            <Option>Burritos</Option>
+            <Option>Soups</Option>
+            <Option>Sides</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Items:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 1.3em;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  margin: 1.3em;
+`;
+const FilterText = styled.span`
+  font-size: 1.3rem;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+export default ProductList;

3de2047cc1b44d411564439b54eca30a11fa4d0a
+ <escobedo.jose48@outlook.com>
2022-07-11 00:42:52 -0700

    adding newsletter component

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -25,6 +25,22 @@ const ProductItem = ({ item }) => {
   );
 };
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 3;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -35,6 +51,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -57,22 +77,12 @@ const Icon = styled.div`
   align-items: center;
   justify-content: center;
   margin: 10px;
+  transition: all 0.5s ease;
 
   &:hover {
     background-color: #e9f5f5;
     transform: scale(1.1);
   }
 `;
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: gray;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 3;
-`;
+
 export default ProductItem;

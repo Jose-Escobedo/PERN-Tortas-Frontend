47abea866dc81b8e43633a1d07a642d9fdb940d5
+ <escobedo.jose48@outlook.com>
2022-07-15 21:13:38 -0700

    added slice to products map for max 7 items

--- src/components/Products.js
+++ src/components/Products.js
@@ -55,7 +55,7 @@ const Products = ({ cat, filters, sort }) => {
         ? filteredProducts.map((item) => {
             return <ProductItem item={item} key={item.id} />;
           })
-        : products.map((item) => {
+        : products.slice(0, 8).map((item) => {
             return <ProductItem item={item} key={item.id} />;
           })}
     </Container>

ad42ea8f9b14a10f32064ac0207b543c9ca77b52
+ <escobedo.jose48@outlook.com>
2022-07-18 23:50:49 -0700

    change headers api calls

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2ZiZDYyMWNjYjE1NGIwYzBjZGY0ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1ODAzNzY5MiwiZXhwIjoxNjU4MTI0MDkyfQ.B4iQZmiQN2ljC8Xye_Vzw2W6-SVBowR0_LYi6ZCnBm0";
-
+const TOKEN = JSON.parse(
+  JSON.parse(localStorage.getItem("persist:root")).currentUser
+).accessToken;
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });

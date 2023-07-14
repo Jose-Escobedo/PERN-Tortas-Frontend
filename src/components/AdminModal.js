540fa470091b6a7c933876847e3c85a55b7727a3
+ <escobedo.jose48@outlook.com>
2023-07-13 23:48:56 -0700

    added sockets to admin page to play sound when new order arrives

---
+++ src/components/AdminModal.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import styled from "styled-components";
+
+const AdminModal = ({ open, close }) => {
+  if (!open) return null;
+  return (
+    <Overlay onClick={close}>
+      <ModalContainer>
+        <NoImageFilterMenuItems>
+          <CloseButton>
+            <AiOutlineClose style={{ fontSize: "2rem", cursor: "pointer" }} />
+          </CloseButton>
+          <h1 className="select-item">SOUND CHECK</h1>
+          <h3>Click the X to enable sound</h3>
+        </NoImageFilterMenuItems>
+      </ModalContainer>
+    </Overlay>
+  );
+};
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 20;
+  left: 0;
+  top: 0;
+`;
+const ModalContainer = styled.div`
+  z-index: 20;
+  width: 50%;
+  height: 20vh;
+  overflow: scroll;
+  overflow-x: hidden;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  padding: 20px;
+  transform: translate(-50%, -50%);
+  display: flex;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.75);
+  @media screen and (max-width: 930px) {
+    width: 70%;
+  }
+  @media screen and (max-width: 580px) {
+    width: 90%;
+  }
+`;
+
+const CloseButton = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 90%;
+  @media screen and (max-width: 450px) {
+    svg {
+      height: 35px;
+      width: 35px;
+    }
+  }
+`;
+
+const NoImageFilterMenuItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+
+  .select-item {
+    padding-bottom: 10px;
+  }
+  img {
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+  }
+  h2 input {
+    margin-right: 1em;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    color: black;
+    cursor: pointer;
+    padding-left: 7px;
+  }
+
+  input {
+    width: 16px;
+    height: 16px;
+    vertical-align: middle;
+    position: relative;
+    *overflow: hidden;
+  }
+  @media screen and (max-width: 450px) {
+    img {
+      width: 35px;
+      height: 35px;
+    }
+    h2 {
+      font-size: 1.1rem;
+    }
+  }
+`;
+const MenuItemComboWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  input[type="radio"] {
+    margin-right: 10px;
+  }
+  label {
+    font-size: 1.5rem;
+    padding: 10px 0;
+  }
+  @media screen and (max-width: 1105px) {
+    label {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media screen and (max-width: 930px) {
+    label {
+      font-size: 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 815px) {
+    label {
+      font-size: 1rem;
+    }
+  }
+  @media screen and (max-width: 815px) {
+    label {
+      font-size: 0.8rem;
+    }
+  }
+`;
+
+export default AdminModal;
--- src/pages/admin/AdminHome.js
+++ src/pages/admin/AdminHome.js
@@ -5,10 +5,14 @@ import SmallWidget from "./SmallWidget";
 import LargeWidget from "./LargeWidget";
 import FeaturedInfo from "./FeaturedInfo";
 import AdminNavbar from "./AdminNavbar";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { io } from "socket.io-client";
+import sound from "./Assets/smb_powerup.wav";
+import AdminModal from "../../components/AdminModal";
+const socket = io.connect("http://localhost:5000");
 
 const AdminHome = () => {
   const user = useSelector((state) => state.user.currentUser);
@@ -18,7 +22,33 @@ const AdminHome = () => {
     JSON.parse(localStorage.getItem("persist:root"))?.user || "{}"
   )?.currentUser?.accessToken;
 
-  useEffect(() => {
+  const [openModal, setOpenModal] = useState(true);
+
+  const play = () => {
+    new Audio(sound).play();
+  };
+
+  // useEffect(() => {
+  //   if (admin) {
+  //     fetch("http://localhost:5000/api/orders/?new=true", {
+  //       method: "GET",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //         authorization: "Bearer " + TOKEN,
+  //       },
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setRecentOrders(data);
+  //       });
+  //   } else {
+  //     console.log("ERROR 404");
+  //   }
+  // }, []);
+  function pollingServerForOrders(curr) {
+    console.log("Checking for orders...");
+
     if (admin) {
       fetch("http://localhost:5000/api/orders/?new=true", {
         method: "GET",
@@ -35,16 +65,33 @@ const AdminHome = () => {
     } else {
       console.log("ERROR 404");
     }
+  }
+
+  useEffect(() => {
+    pollingServerForOrders();
+    socket.on("NewOrder", () => {
+      console.log("New Order!");
+      pollingServerForOrders();
+      play();
+    });
   }, []);
 
   return (
     <>
       <AdminNavbar />
       <AdminContainer>
+        <AdminModal open={openModal} close={() => setOpenModal(false)} />
         <Sidebar />
         <div className="home">
           <FeaturedInfo />
           <AdminOrderContainer>
+            <Button
+              id="modalbtn1"
+              className="btn"
+              onClick={() => setOpenModal(true)}
+            >
+              Order Sound
+            </Button>
             <AdminOrderWrapper>
               {recentOrders.map((order, index) => {
                 return (
@@ -93,6 +140,29 @@ const AdminContainer = styled.div`
     display: flex;
     margin: 20px;
   }
+`;
+const Button = styled.button`
+  display: none;
+  /* display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  border-radius: 10px;
+  &:hover {
+    border: 1px solid aquamarine;
+    transition: all 0.5s ease-in-out;
+    background-color: black;
+    h2 {
+      color: white;
+    }
+    &:first-child {
+      color: white;
+    }
+  }
+  &:first-child {
+    color: teal;
+    font-size: 1rem;
+  } */
 `;
 
 const AdminOrderContainer = styled.div``;
